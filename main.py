2f2df1338c10e1c374c66e08fc6ac0e25bd255b8
Jerzy R <jerzy.roslon@pw.edu.pl>
2023-05-29 14:47:37 +0200

    Add additional arguments for sum and multiply

--- main.py
+++ main.py
@@ -1,17 +1,20 @@
 import logging
 logging.basicConfig(level=logging.DEBUG)
 
-def suma(x, y):
-    logging.info(f'Dodaję {x} i {y}')
-    print(f'Wynik to {x + y}')
+def suma(l):
+    logging.info(f'Dodaję do siebie liczby z listy {l}')
+    print(f'Wynik to {sum(l)}')
 
 def roznica(x, y):
     logging.info(f'Odejmuję {y} od {x}')
     print(f'Wynik to {x - y}')
 
-def mnozenie(x, y):
-    logging.info(f'Mnożę {x} przez {y}')
-    print(f'Wynik to {x * y}')
+def mnozenie(l):
+    wynik = 1
+    logging.info(f'Mnożę przez siebie liczby z listy {l}')
+    for x in l:
+        wynik = wynik * x
+    print(f'Wynik to {wynik}')
 
 def dzielenie(x, y):
     logging.info(f'Dzielę {x} przez {y}')
@@ -23,11 +26,13 @@ if __name__ == "__main__":
     druga = float(input('Podaj drugą liczbę: '))
 
     if dzialanie == 1:
-        suma(pierwsza, druga)
+        wiecej = [pierwsza, druga] + list(map(float, input("Możesz wprowadzić dodatkowe składniki sumy, oddzielaj je spacją: ").split()))
+        suma(wiecej)
     elif dzialanie == 2:
         roznica(pierwsza, druga)
     elif dzialanie == 3:
-        mnozenie(pierwsza, druga)
+        wiecej = [pierwsza, druga] + list(map(float, input("Możesz wprowadzić dodatkowe składniki mnożenia, oddzielaj je spacją: ").split()))
+        mnozenie(wiecej)
     elif dzialanie == 4:
         dzielenie(pierwsza, druga)
     else:
